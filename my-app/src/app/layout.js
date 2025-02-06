655026f447a8ea03ed3517913b7645d0f68e6fed
bilal ahmad <bilalkhann301dir@gamil.com>
2025-02-06 16:59:50 +0500

    fix: replace Script component with standard script tag for Google AdSense in layout

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -22,12 +22,8 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         {/* Google AdSense Script */}
-        <Script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4779738061304824"
-          crossOrigin="anonymous"
-          strategy="beforeInteractive"
-        />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4779738061304824"
+     crossorigin="anonymous"></script>
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
